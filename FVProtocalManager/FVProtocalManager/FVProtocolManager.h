3b5d5e1c14dce04a8ef5fc8b47d429e5b8a37630
Vine <1953892532@qq.com>
2016-12-31 11:48:43 +0800

    文件内容

---
+++ FVProtocalManager/FVProtocalManager/FVProtocolManager.h
@@ -0,0 +1,15 @@
+//
+//  FVProtocolManager.h
+//  FVProtocalManager
+//
+//  Created by mac on 16/12/31.
+//  Copyright © 2016年 SunQL. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FVProtocolManager : NSObject
+
+-(void)test;
+
+@end
---
+++ FVProtocalManager/FVProtocalManager/FVProtocolManager.m
@@ -0,0 +1,15 @@
+//
+//  FVProtocolManager.m
+//  FVProtocalManager
+//
+//  Created by mac on 16/12/31.
+//  Copyright © 2016年 SunQL. All rights reserved.
+//
+
+#import "FVProtocolManager.h"
+
+@implementation FVProtocolManager
+-(void)test{
+    NSLog(@"这是一个测试文件");
+}
+@end
